9b86b0db04a160abd33ed0edea15262bddeb28f1
Wallace Leandro <wallaceleandro2003@gmail.com>
2023-10-26 17:09:08 -0300

    Scroll up

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
 import Header from '../src/components/Header/Header'
+import React, { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    const handleScrollToTop = () => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
+    const links = document.querySelectorAll('a');
+    links.forEach(link => {
+      link.addEventListener('click', handleScrollToTop);
+    });
+
+    return () => {
+      links.forEach(link => {
+        link.removeEventListener('click', handleScrollToTop);
+      });
+    };
+  }, []);
 
   return (
     <>
